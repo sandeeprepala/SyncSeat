2931bcf3e1916f6bb830f5e059034b3af8ae71f5
sandeeprepala <sandeeprepala3@gmail.com>
2026-03-04 02:49:38 +0530

    Production changes

--- Backend/api-gateway/controllers/authController.js
+++ Backend/api-gateway/controllers/authController.js
@@ -25,7 +25,7 @@ export const signup = async (req, res) => {
   res.cookie("token", token, {
   httpOnly: true,
   secure: true,   // true in production (HTTPS)
-  sameSite: true,
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000
 })
 
@@ -52,7 +52,7 @@ export const login = async (req, res) => {
   res.cookie("token", token, {
   httpOnly: true,
   secure: true,
-  sameSite: true,
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000
 })
 
